25747b462a492f22642827b4d8fbb1cceebb9e9a
Vladyslav Moisieienkov <vladyslav.moisieienkov@gmail.com>
2019-10-27 02:59:05 +0200

    Save progress

--- control_system.py
+++ control_system.py
@@ -1,9 +1,29 @@
+import cv2
 import json
 import time
+from tensorflow import keras
 import urllib.request
+
+MAX_SPEED = 1023
+DEFAULT_SPEED = 800
+MINIMAL_SPEED = 750
 
 train_base_address = 'http://192.168.0.180'
 track_base_address = 'http://192.168.0.100:5000'
+
+scap = cv2.VideoCapture('http://192.168.0.190:8080/stream/video.mjpeg')
+
+json_file = open('model/model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+
+loaded_model = keras.models.model_from_json(loaded_model_json)
+loaded_model.load_weights('model/model.h5')
+print('Loaded model from disk')
+
+
+def crop_frame(frame):
+    pass
 
 
 def set_speed(speed: int):
@@ -14,62 +34,63 @@ def set_speed(speed: int):
     query_string = urllib.parse.urlencode(params)
 
     train_speed_url = train_speed_url + '?' + query_string
-    with urllib.request.urlopen(train_speed_url) as response:
-        response_text = response.read()
-    return speed
-
-
-def get_gyroscope_data():
-    pass
+    urllib.request.urlopen(train_speed_url)
 
 
 def get_track_data():
     with urllib.request.urlopen(f'{track_base_address}/rest/items') as response:
-        response_text = response.read()
-        return json.loads(response_text, encoding='utf-8')
+        return json.loads(response.read(), encoding='utf-8')
 
 
-def find_train_position(sensors):
+def find_activated_sensors(sensors):
     sensors_activated = []
-    for id, value in sensors.items():
+    for sensor_id, value in sensors.items():
         if value['state'] == 0:
-            sensors_activated.append(int(id))
+            sensors_activated.append(int(sensor_id))
     return sensors_activated
 
 
-def suggest_speed_on_position(train_position_sensor):
-    print(train_position_sensor)
-    if train_position_sensor in [33, 25]:
-        print('max speed')
-        return int(default_speed * 1.2)
-    elif train_position_sensor in [34, 35, 11]:
-        print('min speed')
-        return int(default_speed)
-    elif train_position_sensor in [31]:
-        print('stop 32')
-        set_speed(0)
-        time.sleep(2)
-        return int(default_speed)
+def get_leading_position(sensors_activated, prev_sensors):
+    result = list(set(sensors_activated) - set(prev_sensors))
+    if len(result):
+        return result[0]
+    return None
+
+
+def position_to_speed(position):
+    if position in [33, 25, 12, 26]:
+        return MAX_SPEED
+    elif position in [31, 34, 13, 11, 24, 32, 27, 23]:
+        return DEFAULT_SPEED
+    elif position in [21]:
+        return MINIMAL_SPEED
     else:
-        print('default speed')
-        return default_speed
+        return MINIMAL_SPEED
 
 
-default_speed = 700
-current_speed = default_speed
-prev_speed = default_speed
+def should_camera_be_activate(position):
+    return position in [32, 21, 13, 11, 24, 23, 22]
 
-paths = []
+
+prev_activated_sensors = []
 
 while True:
     track_data = get_track_data()
-    positions = find_train_position(track_data['track']['rail_sections'])
-    print(positions)
-    current_speed = suggest_speed_on_position(positions[0])
-    set_speed(current_speed)
-    time.sleep(0.3)
+    activated_sensors = find_activated_sensors(track_data['track']['rail_sections'])
+    #if len(activated_sensors) != 0:
+    #    print(activated_sensors)
+    new_leading_position = get_leading_position(activated_sensors, prev_activated_sensors)
+    if new_leading_position:
+        if should_camera_be_activate(new_leading_position):
+            pass
+        print(new_leading_position)
+        speed = position_to_speed(new_leading_position)
+        print(speed)
+    #set_speed(current_speed)
+    prev_activated_sensors = activated_sensors
+    time.sleep(0.1)
 # 800 on turns
 # 700 on
 #set_speed(700)
 #time.sleep(20)
-set_speed(0)
+
